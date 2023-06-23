17c3fc77e2d5eb51aed0daa1f6f8e9c764f8314e
Little-F1sh <itsnihon1@gmail.com>
2023-06-24 02:59:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// Class - 17
+
+// function theFan () {
+//     console.log("Press the switch to turn of the fan")
+
+// }
+// theFan();
+
+// function bringSomeFood (taka) {
+    
+//     var priceOfPotato =20;
+//     var quantityOfPotato = taka/priceOfPotato;
+//     return quantityOfPotato;
+// }
+
+// var totalPotato = bringSomeFood (100);
+// console.log(totalPotato);
+
+// function sumOfNum (num1, num2) {
+//     // console.log(num1, num2);
+//     var total = num1 + num2;
+//     // console.log(total);
+//     return total;
+// }
+
+// var result = sumOfNum (1,2);
+//  console.log(result); 
+
+// function multiplyOfNum(num1, num2, num3) {
+//     var multiply = num1 * num2 *num3;
+//     return multiply;
+// }
+
+// var result = multiplyOfNum (10,20, 2);
+// console.log(result);
+
+
+// function devitionOfNum (num1, num2) {
+//     var devition = num1 / num2;
+//     return devition;
+
+// }
+// var result = devitionOfNum (200, 20);
+// console.log(result);
+
+
+// var student = {   
+//     orderId: 400000,
+//     name : "Mr.x",
+//     gender: "Male",
+//     salary: "1000$"
+// }
+// console.log(student);
+
+// var myPc = {
+//     Brand: "custom",
+//     Cpu: "amd ryzen 5 5600",
+//     Ram : "8gb",
+//     Gpu : "6gb rtx 2060",
+//     Price: 850
+// }
+
+// // myPc.Price = "850$";
+// myPc["Price"] ="850$";
+// console.log(myPc);
